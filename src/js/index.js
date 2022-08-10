75f4390942eb1077f797559b29e6d4577e54c7be
Elirrez8 <elirrez8@gmail.com>
2022-08-10 03:06:49 +0000

    finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,11 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let sec=0;
+setInterval(() => {
+    if(sec >= 1000){sec=0;} /*al llegar a mil se reiniciara, escribe 10 y pruebalo*/
+  sec += 1;
+
+ReactDOM.render( <Home sec ={sec}/>, document.querySelector("#app")
+);
+}, 500);/*un seg tiene 1000 miliseg, 500 x cuestiones visuales */
